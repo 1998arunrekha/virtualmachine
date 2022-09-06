2880a8e60b1f87614e86ca0b5c381aab4eceff38
1998arunrekha <arunar16061998@gmail.com>
2022-09-06 15:44:57 +0530

    second commit

--- main.tf
+++ main.tf
@@ -1,38 +1,38 @@
-resource "azurerm_resource_group" "vm" {
+resource "azurerm_resource_group" "rg" {
   name     = "firstresource"
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "rg" {
-  name                = "resourcegroup"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet01"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.vm.location
-  resource_group_name = azurerm_resource_group.vm.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "firstsubnet"
-  resource_group_name  = azurerm_resource_group.vm.name
-  virtual_network_name = azurerm_virtual_network.vm.name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "app_interface" {
   name                = "app_interface"
-  location            = azurerm_resource_group.vm.location
-  resource_group_name = azurerm_resource_group.vm.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.firstsubnet.id
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_windows_virtual_machine" "wm" {
-  name                = "wvmachine"
-  resource_group_name = azurerm_resource_group.vm.name
-  location            = azurerm_resource_group.vm.location
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = "vm01"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   size                = "Standard_F2"
   admin_username      = "admin"
   admin_password      = "Aruna@123456"
