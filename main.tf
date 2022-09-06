4658af9f2e8f50689fe542b4d2b9a719898d5b8e
1998arunrekha <arunar16061998@gmail.com>
2022-09-06 15:24:55 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,55 @@
+resource "azurerm_resource_group" "vm" {
+  name     = "firstresource"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "rg" {
+  name                = "resourcegroup"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.vm.location
+  resource_group_name = azurerm_resource_group.vm.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "firstsubnet"
+  resource_group_name  = azurerm_resource_group.vm.name
+  virtual_network_name = azurerm_virtual_network.vm.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "app_interface" {
+  name                = "app_interface"
+  location            = azurerm_resource_group.vm.location
+  resource_group_name = azurerm_resource_group.vm.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.firstsubnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "wm" {
+  name                = "wvmachine"
+  resource_group_name = azurerm_resource_group.vm.name
+  location            = azurerm_resource_group.vm.location
+  size                = "Standard_F2"
+  admin_username      = "admin"
+  admin_password      = "Aruna@123456"
+  network_interface_ids = [
+    azurerm_network_interface.app_interface.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
+
